1ab2de2c2f216f9c471a858ef24bd558c5436851
zmc <343512837@qq.com>
2016-01-14 16:13:26 +0800

    添加点击抖动的动画效果

--- 自定义tabbar/CustomTabbar/CustomTabbar.m
+++ 自定义tabbar/CustomTabbar/CustomTabbar.m
@@ -13,6 +13,7 @@
 #define kWindowHeigth [UIScreen mainScreen].bounds.size.height
 #define kNaviGationBarHeight 64
 #define kTabBarHeight 49
+#define angle2Radian(angle) ((angle) / 180.0 * M_PI)
 @implementation CustomTabbar
 -(instancetype)initWithTitles:(NSArray*)titles images:(NSArray*)images selectedImages:(NSArray*)selectedImgaes tabBarStyle:(CustomTabbarStyle)tabBarStyle norTextColor:(UIColor*)norTextColor selectedTextColor:(UIColor*)selectedTextColor bgColor:(UIColor*)bgColor selectedColor:(UIColor*)selectedColor;{
     if (self=[super init]) {
@@ -71,8 +72,10 @@
         btn.selected=YES;
         _lastBtn.selected=NO;
         _lastBtn=btn;
+        [self addCAkeyAnimation];
     }
 }
+
 //设置背景色
 - (void) setBgColor:(UIColor *)bgColor{
     self.backgroundColor=bgColor;
@@ -83,5 +86,20 @@
         btn.titleLabel.font=tabBarFont;
     }
 }
+#pragma mark-点击tabbar抖动动画
+- (void) addCAkeyAnimation{
+    CAKeyframeAnimation  *keyAnima = [CAKeyframeAnimation animation];
+    keyAnima.keyPath = @"transform.rotation";
+    // 度数 / 180 * M_PI
+    keyAnima.values = @[@(-angle2Radian(4)),@(angle2Radian(0)),@(angle2Radian(4)),@(angle2Radian(0))];
+    keyAnima.removedOnCompletion = NO;
+    keyAnima.fillMode = kCAFillModeForwards;
+    keyAnima.duration = 0.2;
+    
+    // 设置动画重复的次数
+    keyAnima.repeatCount = 0;
+    // 2.添加核心动画
+    [_lastBtn.layer addAnimation:keyAnima forKey:nil];
 
+}
 @end
--- 自定义tabbar/TwoViewController.m
+++ 自定义tabbar/TwoViewController.m
@@ -16,7 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     self.view.backgroundColor=[UIColor cyanColor];
     self.navigationItem.title=@"ONE";
 }
